ab96a3b34115a33195bf22298e7aec3db425b70a
bala-amypo <bala@amypo.in>
2025-12-18 08:38:54 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceOwnershipRecord.java
+++ src/main/java/com/example/demo/model/DeviceOwnershipRecord.java
@@ -1,5 +1,14 @@
 package com.example.demo.model;
+import jakarta.persistence.id;
 
 public class DeviceOwnershipRecord {
-    
+    @Id
+    private long id;
+    @column(unique=true)
+    private String serialNumber;
+    private String ownerName;
+    private String ownerEmail;
+    private LocalDate purchaseDate;
+    private LocalDare warrantyExpiration;
+    private Boolean active;
 }
