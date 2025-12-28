321515e63e4e7834bc704abb9d9153a1afb1ea9d
bala-amypo <bala@amypo.in>
2025-12-28 19:40:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/WarrantyClaimController.java
+++ src/main/java/com/example/demo/controller/WarrantyClaimController.java
@@ -12,20 +12,3 @@ public class WarrantyClaimController {
     }
 }
 
-Dto:
-authrq:
-
-
-
-package com.example.demo.dto;
-
-public class AuthRequest {
-    private String email;
-    private String password;
-    
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-}
